79427a8d7716499c7414b9d70a1d12907e0b93a5
dc-sites <dilzvibe@gmail.com>
2026-06-07 14:04:08 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then(reg => console.log('Service Worker registered'))
+            .catch(err => console.log('Service Worker registration failed'));
+    });
+}
+
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', function() {
+        document.querySelectorAll('.nav-item').forEach(i => i.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
